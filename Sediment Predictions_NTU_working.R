0e4e416b614af5506aab20ea3400fdc0d2cb9a31
HBRC\Ashton.Eaves <ashton.eaves@hbrc.govt.nz>
2023-08-17 15:52:18 +1200

    Working but something fshy with SSC to NTU relationship.

--- Sediment Predictions_NTU_working.R
+++ Sediment Predictions_NTU_working.R
@@ -21,11 +21,13 @@ SiteList(dfile, "")
 
 #ISCO sites 
 #Sites <-  c( "Tutaekuri River at Puketapu HBRC Site", "Tukituki River at Red Bridge", "Karamu Stream at Floodgates", "Esk River at Waipunga Bridge", "Mangaone River at Rissington", "Maraetotara River at Waimarama Road")
-Sites <- "Tukituki River at Red Bridge" 
+Sites <- "Karamu Stream at Floodgates" 
 
 ################################################################################
 #Measurements/data that we want to pull from the Hilltop file 
 measurement <- c(	'Suspended Solids [Suspended Solids]','Suspended Sediment Concentration', "Turbidity (FNU) [Turbidity (FNU)]")  
+
+#measurement <- c(	'Suspended Solids [Suspended Solids]','Suspended Sediment Concentration', "Turbidity (FNU) [Turbidity FNU (lab)]")  
 
 Hilltop::SiteList(dfile)
 # Date range. 'date2' = todays date 
@@ -69,6 +71,7 @@ melt <- as.data.frame(sapply(melt, gsub, pattern = "<|>", replacement = ""))
 # Use dplyr to filter the 'melt' table and extract SSC and flow data 
 SSC <- filter(melt, Measurement %in% c("Suspended Sediment Concentration", "Suspended Solids"))
 Value <- filter(melt, Measurement == "Turbidity (FNU)")
+#Value <- filter(melt, Measurement == "Turbidity FNU (lab)")
 #Flow$SampleTaken1 <-  lubridate::floor_date(Flow$SampleTaken, "15 minutes")
 
 
@@ -100,8 +103,8 @@ merged1$Time1 <- format(merged1$DateTime, format = "%I:%M:%S %p")
 
 
 merged1$Value <- as.numeric(merged1$Value)
-merged1$Value <- formatC(merged1$Value, digits = 0, format = "f")
-merged1$Value <- as.character(merged1$Value)
+#merged1$Value <- formatC(merged1$Value, digits = 2, format = "f")
+#merged1$Value <- as.numeric(merged1$Value)
 merged1$Conc <- as.numeric(merged1$Conc)
 merged1$new <- ""
 
@@ -118,9 +121,9 @@ nrow(Final)
 #print(Final[201:296,],right=T, row.names = FALSE)
 
 
-setwd('I:/306 HCE Project/R_analysis/2023analysis/output')
-xlsx::write.xlsx(merged1, 'merged1NTU.xlsx')
-xlsx::write.xlsx(melt, 'melt.xlsx')
+#setwd('I:/306 HCE Project/R_analysis/2023analysis/output')
+#xlsx::write.xlsx(merged1, 'merged1NTU.xlsx')
+#xlsx::write.xlsx(melt, 'melt.xlsx')
 
 merged1$Value <- as.numeric(merged1$Value)
 merged1$Conc <- as.numeric(merged1$Conc)
@@ -128,30 +131,52 @@ merged1$Conc <- as.numeric(merged1$Conc)
 summary(merged1$Value)
 
 
-merged1$Value <- as.numeric(merged1$Value )
-
 Final$merged1.Value <- as.numeric(Final$merged1.Value)
 summary(merged1$Value)
+
+###### Add filter to remove outliers
+
+# Calculate the mean and standard deviation of the values
+mean_value <- mean(merged1$Value)
+sd_value <- sd(merged1$Value)
+
+# Define a threshold for identifying high outliers (e.g., 3 times the standard deviation)
+#threshold <- mean_value + 3 * sd_value
+threshold <- 200
+
+# Subset the data frame to remove rows with high outliers
+filtered_df <- merged1[merged1$Value <= threshold, ]
+
+# Print the filtered data frame
+#print(filtered_df)
 
 #_________________________________________________________________________________________
 # Graph SSC vs turbidity
 
 # Fit the linear regression model
-lm_model <- lm(Conc ~ Value, data = merged1)
+lm_model <- lm(Conc ~ Value, data = filtered_df)
 
-p <- ggplot(data = merged1, aes(x = Value, y = Conc)) +
-  geom_point(size = 4, color = "blue") +    # Customize the points
+# Get R-squared value from the model summary
+r_squared <- summary(lm_model)$r.squared
+
+p <- ggplot(data = filtered_df, aes(x = Value, y = Conc)) +
+  geom_point(size = 2, color = "blue") +    # Customize the points
+  scale_y_continuous() +
+  scale_x_continuous() +
   geom_smooth(method = "lm", se = TRUE, color = "red") +  # Add regression
-  labs(title = "Sediment Scatter Plot with Best-Fit Regression Line",   # Add title
+  labs(title = paste("Sediment Scatter Plot with Best-Fit Regression Line -", Sites), # Add title
        x = "Value (FNU)",                 # X-axis label
-       y = "Conc (mg/l)") +               # Y-axis label
+       y = "SSC (mg/l)") +               # Y-axis label
   geom_text(aes(x = max(Value), y = max(Conc),
                 label = paste("y =", round(lm_model$coefficients[1], 2), "+", 
                               round(lm_model$coefficients[2], 2), "x")),
-            color = "black", hjust = 1, vjust = -1) +
+            color = "black", hjust = 1.75, vjust = 1) +
+  geom_text(x = Inf, y = Inf,  # Position the text in the upper right corner
+            label = paste("R-squared =", round(r_squared, 3)),
+            hjust = 2, vjust = 5, nudge_x = -0.2, nudge_y = -0.2,
+            size = 4, color = "black")
   theme_minimal()  
 p
-
 ggplotly(p)
 
 # Convert the ggplot object to a plotly object
